3a07f80ceaf0f2205728628d0a238910e0b0de91
Karel Fourie <karelmakazi@gmail.com>
2020-07-15 10:08:58 +1200

    Expand button source

--- server/public/data/source.js
+++ server/public/data/source.js
@@ -4,17 +4,85 @@
 //BUTTONS
 const buttonSource = [
   {pageID: 'home',
-    buttonList: ['Our Process', 
+    buttonList: ['Home',
+                  'Preschools', 
+                  'Portraits', 
+                  'Events',
+                  'Our Process',
+                  'Make a Booking'],
+    buttonLinks: ['/',
+                  '/Preschools',
+                  '/Portraits',
+                  '/Events',
+                  '/Our Process',
+                  '/Bookings']
+  },
+  
+  {pageID: 'preschools',
+    buttonList: ['Home',
+                  'Portraits', 
+                  'Events',
+                  'Our Process',
+                  'Make a Booking'],
+    buttonLinks: ['/',
+                  '/Portraits',
+                  '/Events',
+                  '/Our Process',
+                  '/Bookings',]
+    },
+  
+  {pageID: 'portraits',
+    buttonList: ['Home',
+                  'Preschools', 
+                  'Events',
+                  'Our Process',
+                  'Make a Booking'],
+    buttonLinks: ['/',
+                  '/Preschools',
+                  '/Events',
+                  '/Our Process',
+                  '/Bookings',]
+  },
+  
+  {pageID: 'events',
+    buttonList: ['Home',
+                  'Preschools', 
+                  'Portraits', 
+                  'Our Process',
+                  'Make a Booking'],
+    buttonLinks: ['/',
+                  '/Preschools',
+                  '/Portraits',
+                  '/Our Process',
+                  '/Bookings']
+  },
+  
+  {pageID: 'process',
+    buttonList: ['Home',
                   'Preschools', 
                   'Portraits', 
                   'Events',
                   'Make a Booking'],
-    buttonLinks: ['/Process',
+    buttonLinks: ['/',
                   '/Preschools',
                   '/Portraits',
                   '/Events',
                   '/Bookings']
-  }
+  },
+  
+  {pageID: 'booking',
+    buttonList: ['Home',
+                  'Preschools', 
+                  'Portraits', 
+                  'Events',
+                  'Our Process'],
+    buttonLinks: ['/',
+                  '/Preschools',
+                  '/Portraits',
+                  '/Events',
+                  '/Our Process']
+  },
+  
 ]
 
 const copySource = [
