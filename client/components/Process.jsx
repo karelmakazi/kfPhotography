73e26f01113fa4ee5ded2df921724118bfeeabd5
Karel Fourie <karelmakazi@gmail.com>
2020-07-29 10:15:47 +1200

    Update navigation

--- client/components/Process.jsx
+++ client/components/Process.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class Process extends Component {
   render() {
     return (
+      <>
       <div>
         <h1> Bookings </h1>
       </div>
       <div className='contentWrapper'>
-        
+
       </div>
+      </>
     );
   }
 }
