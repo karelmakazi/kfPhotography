f9996ae847d55c065a1fe71fc8915c3d194ac041
Karel Fourie <karelmakazi@gmail.com>
2020-07-30 11:57:45 +1200

    Format title

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -16,15 +16,41 @@ const Navigation = props => {
       <div className='headerWrapper'>
         <div className='logoWrapper'>
 
+          <div className='headerStripLeft'>
+            <div className='headerTier1'></div>
+            <div className='headerTier2'></div>
+            <div className='headerTier3'></div>
+            <div className='headerTier4'></div>
+            <div className='headerTier3'></div>
+            <div className='headerTier2'></div>
+            <div className='headerTier1'></div>
+          </div>
+
+          <div className='headerTitleBlock'>
+            <span className='topTitle'>Karel Fourie</span><br></br>
+            <span className='mainTitle'>Photography</span>
+          </div>
+
+          <div className='headerStripRight'>
+            <div className='headerTier1'></div>
+            <div className='headerTier2'></div>
+            <div className='headerTier3'></div>
+            <div className='headerTier4'></div>
+            <div className='headerTier3'></div>
+            <div className='headerTier2'></div>
+            <div className='headerTier1'></div>
+          </div>
+
         </div>
+
+
         <div className='navWrapper'>
-        {buttonList.map((text, index) => {
-        return (
-        <Link to={buttonLinks[index]} key={index}>
-          <button className='' value=''> {text} </button>
-        </Link>
-  )
- })}
+          {buttonList.map((text, index) => {
+          return (
+            <Link to={buttonLinks[index]} key={index}>
+              <button className='' value=''> {text} </button>
+            </Link>
+            )})}
         </div>
       </div>
     )
